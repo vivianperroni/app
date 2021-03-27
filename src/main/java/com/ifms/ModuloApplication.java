77afbe3a0902a2a9e4fb873a7c8f17a8b1b7cc16
vivianperroni <vivian.perroni@hotmail.com>
2021-03-26 22:55:14 -0300

    Testanto Entidade e Recursos

--- src/main/java/com/ifms/ModuloApplication.java
+++ src/main/java/com/ifms/ModuloApplication.java
@@ -1,24 +1,12 @@
 package com.ifms;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ModuloApplication {
-	
-	@Autowired
-	@Qualifier("nameAplication")
-	private String nameAplication;
-
-	@GetMapping("/nome")
-	public String nomeDaAplicacao() {
-		return nameAplication;
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModuloApplication.class, args);
